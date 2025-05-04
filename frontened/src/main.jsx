90e0a9ab061981b680240db53037294e3cb953da
sushil singh <ssk2000gkp@gmail.com>
2025-05-04 21:08:25 +0530

    project done

---
+++ frontened/src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {Toaster} from 'react-hot-toast'
+import {Provider} from 'react-redux';
+import  store  from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+let persistor = persistStore(store);
+
+
+export const BASE_URL = "http://localhost:8080";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <Toaster />
+      </PersistGate>
+    </Provider>
+  </StrictMode>
+);
