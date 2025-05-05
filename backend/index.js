c4931adcbe75f8df76d0f651476dbe0691e7e73b
sushil singh <ssk2000gkp@gmail.com>
2025-05-05 19:56:13 +0530

    url add

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOption = {
-  origin: "http://localhost:5173",
+  origin: "https://chat-application-full-stack-1.onrender.com",
   credentials: true,
 };
 app.use(cors(corsOption));
--- frontened/src/main.jsx
+++ frontened/src/main.jsx
@@ -11,7 +11,7 @@ import { persistStore } from "redux-persist";
 let persistor = persistStore(store);
 
 
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://chat-application-full-stack-1.onrender.com";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
